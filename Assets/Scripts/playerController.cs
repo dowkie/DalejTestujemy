5c3108c909758261714a74030cc081399786cba2
Przemysław Widera <przemyslawwidera@Mac-mini-Przemysaw.local>
2018-02-09 23:33:01 +0100

    moving and jumping
    added moving and jumping

---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour {
+
+	public float speed;
+	public float jump_power;
+
+	private Rigidbody2D rb;
+	private bool jumping = false;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			transform.position = new Vector3 (transform.position.x - speed, transform.position.y, transform.position.z);
+		}
+
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			transform.position = new Vector3 (transform.position.x + speed, transform.position.y, transform.position.z);
+		}
+
+		if (Input.GetKey (KeyCode.Space)) {
+			rb.AddForce (new Vector2 (0, jump_power));
+			jumping = true;
+		}
+	}
+}
